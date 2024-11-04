1779794c33b2f2591dc5936235e9bf1044925684
ahmedDev32 <166875212+ahmedDev32@users.noreply.github.com>
2024-11-04 15:22:47 +0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,14 @@
+
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <link rel="icon" sizes="180x180" href="/images/favicon.svg" />
+      <Head>
+        <meta name="google-adsense-account" content="ca-pub-4844771741097525" />
+        <link rel="icon" sizes="180x180" href="/images/favicon.svg" />
+      </Head>
       <body className="antialiased">
-
         <Main />
         <NextScript />
       </body>
