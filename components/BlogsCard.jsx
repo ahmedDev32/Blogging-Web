196eebaa0c0e7ba1d762aaa91463945c60a14eeb
ahmed_web_expert <166875212+ahmedDev32@users.noreply.github.com>
2025-03-16 02:38:08 +0500

    Update BlogsCard.jsx

--- components/BlogsCard.jsx
+++ components/BlogsCard.jsx
@@ -23,7 +23,10 @@ const BlogsCard = ({item}) => {
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
                     {item.title}
                   </h1>
-                  <p className="leading-relaxed mb-3">{item.orignal_post}</p>
+                  <p className="leading-relaxed mb-3">
+                :earn More About {item.title}
+
+                  </p>
                   <div className="flex items-center flex-wrap">
                     <a className="text-pink-500 inline-flex items-center md:mb-2 lg:mb-0">
                       Learn More
